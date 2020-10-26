479eb6e6d06eaaa2ad03dc7992819d199af976db
Praneethachodey <praneethachodey@gmail.com>
2020-10-26 18:07:53 +0530

    [Praneetha]Edited: to achieve dry code

--- src/main/java/come/bridgelabz/CabService/EnhancedInvoice.java
+++ src/main/java/come/bridgelabz/CabService/EnhancedInvoice.java
@@ -1,6 +1,37 @@
 package come.bridgelabz.CabService;
 
 public class EnhancedInvoice {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(avgFare);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(totalFare);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + totalNoOfRides;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EnhancedInvoice other = (EnhancedInvoice) obj;
+		if (Double.doubleToLongBits(avgFare) != Double.doubleToLongBits(other.avgFare))
+			return false;
+		if (Double.doubleToLongBits(totalFare) != Double.doubleToLongBits(other.totalFare))
+			return false;
+		if (totalNoOfRides != other.totalNoOfRides)
+			return false;
+		return true;
+	}
+
 	int totalNoOfRides;
 	double totalFare;
 	double avgFare;
--- src/test/java/come/bridgelabz/CabService/InvoiceServiceTest.java
+++ src/test/java/come/bridgelabz/CabService/InvoiceServiceTest.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import come.bridgelabz.CabService.Ride.rideType;
 
 public class InvoiceServiceTest {
-
+ InvoiceService invoiceservice = null;
+	
+	@Before
+	public void initialise() {
+		invoiceservice = new InvoiceService();
+	}
 	@Test
 	public void givenDistAndTime_ShouldReturnFare() {
-		InvoiceService invoiceservice = new InvoiceService();
 		double dist = 2.0;
 		int time = 5;
 		double fare = invoiceservice.getFare(dist, time);
@@ -21,7 +26,6 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenLessDistAndTime_ShouldReturnMinFare() {
-		InvoiceService invoiceservice = new InvoiceService();
 		double dist = 0.1;
 		int time = 1;
 		double fare = invoiceservice.getFare(dist, time);
@@ -30,7 +34,6 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenMultipleRides_ShouldReturnTotalFare() {
-		InvoiceService invoiceservice = new InvoiceService();
 		Ride[] rides = { new Ride(2.0, 5, rideType.REGULAR), new Ride(0.1, 1, rideType.REGULAR) };
 		double fare = invoiceservice.getFare(rides);
 		Assert.assertEquals(30, fare, 0.0);
@@ -39,33 +42,27 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenMultipleRides_ShouldReturnEnhancedInvoice() {
-		InvoiceService invoiceservice = new InvoiceService();
 		Ride[] rides = { new Ride(2.0, 5, rideType.REGULAR), new Ride(0.1, 1, rideType.REGULAR) };
 		EnhancedInvoice summary = invoiceservice.getEnhancedInvoice(rides);
-		Assert.assertEquals(30, summary.totalFare, 0.0);
-		Assert.assertEquals(2, summary.totalNoOfRides);
-		Assert.assertEquals(15, summary.avgFare, 0.0);
+		EnhancedInvoice expectedSummary = new EnhancedInvoice(2, 30,15);
+		Assert.assertEquals(expectedSummary,summary);
 	}
 
 	@Test
 	public void givenUserId_ShouldReturnEnhancedInvoice() {
-		InvoiceService invoiceservice = new InvoiceService();
 		Ride[] rides ={ new Ride(2.0, 5,rideType.REGULAR), new Ride(0.1, 1,rideType.REGULAR) };
 		RideRepository.add("praneetha", new ArrayList<Ride>(Arrays.asList(rides)));
 		EnhancedInvoice summary = invoiceservice.getRideFromUserId("praneetha");
-		Assert.assertEquals(30, summary.totalFare, 0.0);
-		Assert.assertEquals(2, summary.totalNoOfRides);
-		Assert.assertEquals(15, summary.avgFare, 0.0);
+		EnhancedInvoice expectedSummary = new EnhancedInvoice(2, 30,15);
+		Assert.assertEquals(expectedSummary,summary);
 	}
 	
 	@Test
 	public void givenUserIdandPremium_ShouldReturnEnhancedInvoice() {
-		InvoiceService invoiceservice = new InvoiceService();
 		Ride[] rides = { new Ride(2.0, 5, rideType.REGULAR), new Ride(0.1, 1, rideType.PREMIUM) };
 		RideRepository.add("pranee", new ArrayList<Ride>(Arrays.asList(rides)));
 		EnhancedInvoice summary = invoiceservice.getRideFromUserId("pranee");
-		Assert.assertEquals(45, summary.totalFare, 0.0);
-		Assert.assertEquals(2, summary.totalNoOfRides);
-		Assert.assertEquals(22.5, summary.avgFare, 0.0);
+		EnhancedInvoice expectedSummary = new EnhancedInvoice(2, 45,22.5);
+		Assert.assertEquals(expectedSummary,summary);
 	}
 }
